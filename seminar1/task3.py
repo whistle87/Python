a70a0c77cfd531e1b5d15d703a421f376ad077f3
Marina <99289217+whistle87@users.noreply.github.com>
2023-02-26 18:00:35 +0300

    Delete seminar1 directory

--- seminar1/task3.py
+++
@@ -1,3 +0,0 @@
-input_number = int(input("Enter a number n: "))
-print("n + nn + nnn = {}".format(input_number + (input_number + input_number * 10) +
-                                 (input_number + input_number * 10 + input_number * 100)))
