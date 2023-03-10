77442420bbd8ea5078832eebae6bec5aea0cf834
Marina <vehorevama@gmail.com>
2023-03-10 16:00:34 +0300

    Add seminar 6 tasks

---
+++ seminar6/task30.py
@@ -0,0 +1,16 @@
+"""
+Заполните массив элементами арифметической прогрессии.
+Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+Каждое число вводится с новой строки.
+"""
+first_num, diff, el_amount = input("Enter first number, difference and amount of element: ").split()
+try:
+    first_num = int(first_num)
+    diff = int(diff)
+    el_amount = int(el_amount)
+except ValueError:
+    print("It is not a number")
+else:
+    progression = [first_num + (el - 1) * diff for el in range(1, el_amount + 1)]
+    print(progression)
